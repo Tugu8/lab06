5d94a64d755e29e5144805d5f78a5faf3528c061
Tugu8 <tugutugu018@gmail.com>
2026-03-11 14:28:36 +0800

    Extend InheritanceSortedIntList to track additions
    Implemented totalAdded to track number of attempts to add elements.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,40 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+    private int totalAdded = 0;
 
+    /**
+     * Adds the specified int to the list and increments the attempt count.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return super.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list and increments the attempt count.
+     * Prevents double counting elements if super.addAll calls this.add under the hood.
+     *
+    //  * @param list IntegerList containing elements to be added to the list
+    //  * @return true if the list changed as a result of the call
+    //  */
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean result = false;
+        result = super.addAll(list);
+        totalAdded += list.size();
+        return result;
+    }
+
+    /**
+     * @return integer denoting the number of attempted insertions into the list
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
